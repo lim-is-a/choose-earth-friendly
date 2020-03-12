55bb5d2d4faf35012d05f19eb27bcb6a9da8b4fc
Isabel Limas <ilimasc3@gmail.com>
2020-03-12 15:37:37 -0400

    initial commit

---
+++ client/build/precache-manifest.a4eb94ce88a5ec4892ac51310a073707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cff4febaeb8943293455884a04df56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2374dae43999ccabc5de",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "2374dae43999ccabc5de",
+    "url": "/static/js/main.e9710547.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ models/schemas.js
@@ -0,0 +1,44 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * Import mongoose connection
+ *
+ */
+const mongoose = require('./connection.js')
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ *
+ */
+//const SampleModelSchema = new mongoose.Schema({
+//  name: String
+//})
+const ClothesSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    url: String,
+})
+const ShoesSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    url: String,
+})
+const RestaurantsSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    url: String,
+})
+
+/* Step 3
+ *
+ * TODO: export the schema
+ */
+//module.exports = mongoose.model('Sample', SampleModelSchema);
+module.exports = mongoose.model('Clothes', ClothesSchema);
+module.exports = mongoose.model('Shoes', ShoesSchema);
+module.exports = mongoose.model('Restaurants', RestaurantsSchema);
