adc2e3e7b23542db908b2775a596238417040959
Isabel Limas <ilimasc3@gmail.com>
2020-03-17 13:44:12 -0400

    request handlers added to homePage.js

--- controllers/homePage.js
+++ controllers/homePage.js
@@ -35,20 +35,107 @@ const homeRouter = express.Router()
 /* Step 4
  * 
  * TODO: Put all request handlers here
- */
-
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
+ * 
 // templateRouter.get('/', (req, res) => {
 //   res.json('hello world')
 // })
+*/
+// -----------------CLOTHES------------------
+// #INDEX
 homeRouter.get('/', (req, res) => {
-  res.json('hello world')
+  Clothes.find().then((clothes) => {
+    res.json(clothes)
+  })
+})
+// #SHOW
+homeRouter.get('/clothes/:clothesId', (req, res) => {
+  Clothes.findById(req.params.clothesId).then((clothes) => {
+    res.json(clothes)
+  })
+})
+// #CREATE
+homeRouter.post('/', (req, res) => {
+  Clothes.create(req.body).then(() => {
+    res.status(200).end()
+  })
+})
+// #UPDATE
+homeRouter.put('/clothes/:clothesId', (req, res) => {
+  Clothes.findByIdAndUpdate(req.params.clothesId, req.body).then(() => {
+    res.status(200).end()
+  })
+})
+// #DELETE
+homeRouter.delete('/clothes/:clothesId', (req, res) => {
+  Clothes.findByIdAndRemove(req.params.clothesId).then(() => {
+    res.status(200).end()
+  })
+})
+// -----------------SHOES---------------------
+// #SHOW
+homeRouter.get('/shoes/:shoesId', (req, res) => {
+  Shoes.findById(req.params.shoesId).then((s) => {
+    res.json(s)
+  })
+})
+// #CREATE
+homeRouter.post('/shoes', (req, res) => {
+  Shoes.create(req.body).then(() => {
+    res.status(200).end()
+  })
+})
+// #UPDATE
+homeRouter.put('/shoes/:shoesId', (req, res) => {
+  Shoes.findByIdAndUpdate(req.params.shoesId, req.body).then(() => {
+    res.status(200).end()
+  })
+})
+// #DELETE
+homeRouter.delete('/shoes/:shoesId', (req, res) => {
+  Shoes.findByIdAndRemove(req.params.shoesId).then(() => {
+    res.status(200).end()
+  })
+})
+// #INDEX
+homeRouter.get('/shoes', (req, res) => {
+  Shoes.find().then((shoes) => {
+    res.json(shoes)
+  })
+})
+// --------------RESTAURANTS------------------
+// #SHOW
+homeRouter.get('/restaurants/:restaurantId', (req, res) => {
+  Restaurants.findById(req.params.restaurantId).then((restaurants) => {
+    res.json(restaurants)
+  })
+})
+// #CREATE
+homeRouter.post('/restaurants', (req, res) => {
+  Restaurants.create(req.body).then(() => {
+    res.status(200).end()
+  })
+})
+// #UPDATE
+homeRouter.put('/restaurants/:restaurantId', (req, res) => {
+  Restaurants.findByIdAndUpdate(req.params.restaurantId, req.body).then(() => {
+    res.status(200).end()
+  })
+})
+// #DELETE
+homeRouter.delete('/restaurants/:restaurantId', (req, res) => {
+  Restaurants.findByIdAndRemove(req.params.restaurantId).then(() => {
+    res.status(200).end()
+  })
+})
+// #INDEX
+homeRouter.get('/restaurants', (req, res) => {
+  Restaurants.find().then((restaurants) => {
+    res.json(restaurants)
+  })
 })
 
-/* Step 6
+
+/* Step 5
  *
  * Export the router from the file.
  *
