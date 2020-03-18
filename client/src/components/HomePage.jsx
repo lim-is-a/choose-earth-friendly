460a9d9addcd55008b4e2427db0b0f0629d74c59
Isabel Limas <ilimasc3@gmail.com>
2020-03-18 18:38:26 -0400

    Moved navbar state and functions to homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,17 +1,49 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import axios from 'axios'
 
 
 export default class HomePage extends Component {
+    state = {
+        clothesList: [],
+        shoesList: [],
+        restaurantsList: []
+    }
+
+    getClothes = () => {
+        axios.get('/api/earthfriendly/clothes').then((response) => {
+            this.setState({
+                clothesList: response.data
+            })
+        })
+    }
+    getShoes = () => {
+        axios.get('/api/earthfriendly/shoes').then((response) => {
+            this.setState({
+                shoesList: response.data
+            })
+        })
+    }
+    getRestaurants = () => {
+        axios.get('/api/earthfriendly/restaurants').then((response) => {
+            this.setState({
+                restaurantsList: response.data
+            })
+        })
+    }
+
     render() {
         return (
             <div>
-                <Navbar/>
                 <div>
                     <h2>Welcome to:</h2>
                     <h1>Choose Earth Friendly!</h1>
                 </div>
-
+                <Navbar
+                    getClothes={this.getClothes}
+                    getShoes={this.getShoes}
+                    getRestaurants={this.getRestaurants} />
+            
             </div>
         )
     }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,46 +1,16 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 export default class Navbar extends Component {
-    state = {
-        clothesList: [],
-        shoesList: [],
-        restaurantsList: []
-    }
-
-    getClothes = () => {
-        axios.get('/api/earthfriendly/clothes').then((response) => {
-            this.setState({
-                clothesList: response.data
-            })
-        })
-    }
-    getShoes = () => {
-        axios.get('/api/earthfriendly/shoes').then((response) => {
-            this.setState({
-                shoesList: response.data
-            })
-        })
-    }
-    getRestaurants = () => {
-        axios.get('/api/earthfriendly/restaurants').then((response) => { 
-            this.setState({
-                restaurantsList: response.data
-            })
-        })
-     }
 
     render() {
+
         return (
             <div>
                 <nav>
                     <a href="/">Home</a>
-                    <button onClick={() => { this.getClothes() }}>Clothes</button>
-                    <button onClick={() => {this.getShoes() }}>Shoes</button>
-                    <button onClick={() => { this.getRestaurants()}}>Restaurants</button>
-                    {/* <a href="">Clothes</a>
-                    <a href="">Shoes</a>
-                    <a href="">Restaurants</a> */}
+                    <button onClick={() => { this.props.getClothes() } }>Clothes</button>
+                    <button onClick={() => {this.props.getShoes() }}>Shoes</button>
+                    <button onClick={() => { this.props.getRestaurants()}}>Restaurants</button>
                 </nav>
             </div>
         )
