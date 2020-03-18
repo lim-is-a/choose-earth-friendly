db3d23307e002d1b372e5cf0ec93187c60cdf257
Isabel Limas <ilimasc3@gmail.com>
2020-03-18 18:54:39 -0400

    change views on state change

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
 import axios from 'axios'
-
+import Clothes from './Clothes'
+import Restaurants from './Restaurants'
+import Shoes from './Shoes'
 
 export default class HomePage extends Component {
     state = {
@@ -13,20 +15,26 @@ export default class HomePage extends Component {
     getClothes = () => {
         axios.get('/api/earthfriendly/clothes').then((response) => {
             this.setState({
-                clothesList: response.data
+                clothesList: response.data,
+                shoesList: [],
+                restaurantsList: []
             })
         })
     }
     getShoes = () => {
         axios.get('/api/earthfriendly/shoes').then((response) => {
             this.setState({
-                shoesList: response.data
+                shoesList: response.data,
+                clothesList: [],
+                restaurantsList: []
             })
         })
     }
     getRestaurants = () => {
         axios.get('/api/earthfriendly/restaurants').then((response) => {
             this.setState({
+                clothesList: [],
+                shoesList: [],
                 restaurantsList: response.data
             })
         })
@@ -35,15 +43,27 @@ export default class HomePage extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <h2>Welcome to:</h2>
-                    <h1>Choose Earth Friendly!</h1>
-                </div>
+               
                 <Navbar
                     getClothes={this.getClothes}
                     getShoes={this.getShoes}
                     getRestaurants={this.getRestaurants} />
-            
+                 <div>
+                    <h2>Welcome to:</h2>
+                    <h1>Choose Earth Friendly!</h1>
+                </div>
+                {(this.state.clothesList.length > 0)
+                    ? <Clothes/>
+                    : null
+                }
+                {(this.state.shoesList.length > 0)
+                    ? <Shoes/>
+                    : null
+                }
+                {(this.state.restaurantsList.length > 0)
+                    ? <Restaurants/>
+                    : null
+                }
             </div>
         )
     }
