84fe8c3a506f5f4c224e8d350329fc9e1914b849
Isabel Limas <ilimasc3@gmail.com>
2020-03-18 11:20:06 -0400

    edited App.js format

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import HomePage from './components/HomePage.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage}/>
-        </Switch>
-      </Router>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
