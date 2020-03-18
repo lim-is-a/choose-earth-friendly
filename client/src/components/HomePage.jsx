3e33b81b949b0caf253263d987833c4a86079337
Isabel Limas <ilimasc3@gmail.com>
2020-03-18 18:16:48 -0400

    removed state from homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,38 +1,17 @@
 import React, { Component } from 'react'
+import Navbar from './Navbar'
 
 
 export default class HomePage extends Component {
-    state = {
-        clothingPlace: {
-            name: "",
-            description: "",
-            url: ""
-        },
-        restaurant: {
-            name: "",
-            description: "",
-            url: ""
-        },
-        shoePlace: {
-            name: "",
-            description: "",
-            url: ""
-        },
-        clothes: [],
-        restaurants: [],
-        shoes: []
-    }
-
     render() {
         return (
             <div>
-                <h1>Welcome to Choose Earth Friendly!</h1>
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/clothes">Clothes</a>
-                    <a href="/shoes">Shoes</a>
-                    <a href="/restaurants">Restaurants</a>
-                </nav>
+                <Navbar/>
+                <div>
+                    <h2>Welcome to:</h2>
+                    <h1>Choose Earth Friendly!</h1>
+                </div>
+
             </div>
         )
     }
