7c2d59997313bfd7422e039aca7ee25151a9a801
Isabel Limas <ilimasc3@gmail.com>
2020-03-18 11:29:25 -0400

    imported components into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css';
-import HomePage from './components/HomePage.jsx';
-
+import './App.css'
+import HomePage from './components/HomePage.jsx'
+import Clothes from './components/Clothes'
+import Restaurants from './components/Restaurants'
+import Shoes from './components/Shoes'
 
 export default class App extends Component {
   render() {
@@ -11,6 +13,9 @@ export default class App extends Component {
         <Router>
           <Switch>
             <Route exact path="/" component={HomePage} />
+            <Route exact path="/clothes" component={Clothes} />
+            <Route exact path="/restaurants" component={Restaurants} />
+            <Route exact path="/shoes" component={Shoes} />
           </Switch>
         </Router>
       </div>
