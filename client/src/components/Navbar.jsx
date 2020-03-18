b5817674da53da8e0e938ee2d1fda79b27900183
Isabel Limas <ilimasc3@gmail.com>
2020-03-18 18:24:13 -0400

    added functions to get list of clicked

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,31 +3,41 @@ import axios from 'axios'
 
 export default class Navbar extends Component {
     state = {
-        clothesList: []    
+        clothesList: [],
+        shoesList: [],
+        restaurantsList: []
     }
 
-    getClothes = () => { 
-        axios.get('/api/earthfriendly/clothes').then((response) => { 
+    getClothes = () => {
+        axios.get('/api/earthfriendly/clothes').then((response) => {
             this.setState({
                 clothesList: response.data
             })
         })
     }
-
-
-    getShoes = () => { }
-    getRestaurants = () => { }
-
-
+    getShoes = () => {
+        axios.get('/api/earthfriendly/shoes').then((response) => {
+            this.setState({
+                shoesList: response.data
+            })
+        })
+    }
+    getRestaurants = () => {
+        axios.get('/api/earthfriendly/restaurants').then((response) => { 
+            this.setState({
+                restaurantsList: response.data
+            })
+        })
+     }
 
     render() {
         return (
             <div>
-                 <nav>
+                <nav>
                     <a href="/">Home</a>
-                    <button onClick={ () => {this.getClothes()}}>Clothes</button>
-                    <button onClick={ () => {}}>Shoes</button>
-                    <button onClick={ () => {}}>Restaurants</button>
+                    <button onClick={() => { this.getClothes() }}>Clothes</button>
+                    <button onClick={() => {this.getShoes() }}>Shoes</button>
+                    <button onClick={() => { this.getRestaurants()}}>Restaurants</button>
                     {/* <a href="">Clothes</a>
                     <a href="">Shoes</a>
                     <a href="">Restaurants</a> */}
--- client/src/components/Shoes.jsx
+++ client/src/components/Shoes.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Shoes extends Component {
+    // state = {
+    //     shoeList 
+    // }
     render() {
         return (
             <div>
                 <h1>Shoes</h1>
+                {/* <shoe name={} /> */}
             </div>
         )
     }
