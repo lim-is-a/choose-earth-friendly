b099c54edd72008bcf23b4ab53a4fa983aff3207
Isabel Limas <ilimasc3@gmail.com>
2020-03-18 11:21:26 -0400

    added state and links to homepage

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 
 export default class HomePage extends Component {
+    state = {
+        clothingPlace: {
+            name: "",
+            description: "",
+            url: ""
+        },
+        restaurant: {
+            name: "",
+            description: "",
+            url: ""
+        },
+        shoePlace: {
+            name: "",
+            description: "",
+            url: ""
+        },
+        clothes: [],
+        restaurants: [],
+        shoes: []
+    }
+
     render() {
         return (
             <div>
-                <h1>hello from homepage!!</h1>
+                <h1>Welcome to Choose Earth Friendly!</h1>
+                <nav>
+                    <a href="/clothes">Clothes</a>
+                    <a href="/shoes">Shoes</a>
+                    <a href="/restaurants">Restaurants</a>
+                </nav>
             </div>
         )
     }
